333c157c86d5b4429aafd35e787b78c9f0e5c477
cordelia-swift <87986478+cordelia-swift@users.noreply.github.com>
2025-11-18 14:30:30 -0500

    Update run_2.rb

--- school_data/run_2.rb
+++ school_data/run_2.rb
@@ -11,33 +11,9 @@
 ################# INT RUN ###################
 #############################################
 
-# schools: 3 synced successfully
-#   name: My school
-#   grade_low: 9
-#   grade_high: 12
-
 # students: 75 synced successfully; 25 errored
 #  first_name: Amy
 #  last_name: Lou
-#  school: My school
-#  grade: 10
-#  relationships:
-#    - first_name: Johnny
-#      last_name: Lou
-#      relationship: father
-#    - first_name: Missy
-#      last_name: Lou
-#      relationship: father
 
-# faculty:
-#   first_name: Mike
-#   last_name: Smith
-
-# sections: 20 synced successfully
-#   name: "9th Grade Algebra"
-#   teacher: "Mike Smith"
-
-# student_enrollments: 80 synced successully; 20 couldn't find student
-#   id: uuid
-#   student: Amy Lou
-#   9th Grade Algebra
+#  first_name: Amy-Lou
+#  last_name: Smith
